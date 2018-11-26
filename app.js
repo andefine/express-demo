f473113728832c2585f4cb19f1b54e54a612d24b
andefine <1939866551@qq.com>
2018-11-27 00:09:53 +0800

    express 浅析

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ const userRouter = require('./routes/users')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use('/', indexRouter)
-app.use('/users', userRouter)
+// app.use('/', indexRouter)
+// app.use('/users', userRouter)
+
+app.use((req, res, next) => {
+  console.log(1)
+  next(new Error('dhfahf'))
+})
+app.use((req, res, next) => {
+  console.log(2)
+  res.status(200).end()
+})
+app.use((err, req, res, next) => {
+  console.log(err.stack)
+  res.status(500).send('something wrong')
+})
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}`)
