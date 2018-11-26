fba5046010d2ab62eda0f205f4a87517c86c5688
andefine <1939866551@qq.com>
2018-11-26 22:31:16 +0800

    路由

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('hello express')
-})
+const indexRouter = require('./routes/index')
+const userRouter = require('./routes/users')
+
+app.use('/', indexRouter)
+app.use('/users', userRouter)
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}`)
