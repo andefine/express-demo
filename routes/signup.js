d0d4e9fc354ae59494308ab46ba804850918d0d5
andefine <1939866551@qq.com>
2018-11-29 20:28:10 +0800

    注册于文件上传

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+const sha1 = require('sha1')
 const express = require('express')
 const router = express.Router()
 
+const UserModel = require('../models/users')
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 router.get('/', checkNotLogin, function (req, res, next) {
@@ -8,7 +12,66 @@ router.get('/', checkNotLogin, function (req, res, next) {
 })
 
 router.post('/', checkNotLogin, function (req, res, next) {
-  res.send('注册')
+  const name = req.fields.name
+  const gender = req.fields.gender
+  const bio = req.fields.bio
+  const avatar = req.files.avatar.path.split(path.sep).pop()
+  let password = req.fields.password
+  const repassword = req.fields.repassword
+
+  try {
+    if (!(name.length >= 1 && name.length <= 10)) {
+      throw new Error('名字请限制在1-10个字符')
+    }
+    if (['m', 'f', 'x'].indexOf(gender) === -1) {
+      throw new Error('性别只能是m、f或x')
+    }
+    if (!(bio.length >= 1 && bio.length <= 30)) {
+      throw new Error('个人简介请限制在1-30个字符')
+    }
+    if (!req.files.avatar.name) {
+      throw new Error('缺少头像')
+    }
+    if (password.length < 6) {
+      throw new Error('密码至少6个字符')
+    }
+    if (password !== repassword) {
+      throw new Error('两次输入密码不一致')
+    }
+  } catch (e) {
+    fs.unlink(req.files.avatar.path, function (err) {
+      console.log(err)
+    })
+    req.flash('error', e.message)
+    return res.redirect('/signup')
+  }
+
+  password = sha1(password)
+
+  let user = {
+    name,
+    password,
+    gender,
+    bio,
+    avatar
+  }
+
+  UserModel.create(user)
+  .then(result => {
+    user = result.ops[0]
+    delete user.password
+    req.session.user = user
+    req.flash('success', '注册成功')
+    res.redirect('/posts')
+  })
+  .catch(e => {
+    fs.unlink(req.files.avatar.path)
+    if (e.message.match('duplicate key')) {
+      req.flash('error', '用户名已被占用')
+      res.redirect('/signup')
+    }
+    next(e)
+  })
 })
 
 module.exports = router
