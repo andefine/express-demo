3c308629806fcd2e125ec020a45f1a6fec022cfe
andefine <1939866551@qq.com>
2018-12-04 23:34:22 +0800

    登出

--- models/users.js
+++ models/users.js
@@ -3,5 +3,12 @@ const User = require('../lib/mongo').User
 module.exports = {
   create: function (user) {
     return User.create(user).exec()
+  },
+
+  getUserByName (name) {
+    return User
+    .findOne({ name })
+    .addCreateAt()
+    .exec()
   }
 }
--- routes/signin.js
+++ routes/signin.js
@@ -4,11 +4,22 @@ const router = express.Router()
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 router.get('/', checkNotLogin, function (req, res, next) {
-  res.send('登录页')
+  res.render('signin')
 })
 
 router.post('/', checkNotLogin, function (req, res, next) {
   res.send('登录')
+  // const name = req.fields.name
+  // const password = req.fields.password
+
+  // try {
+  //   if (!name.length) {
+  //     throw new Error('请填写用户名')
+  //   }
+  // } catch (e) {
+  //   req.flash('error', e.message)
+  //   return res.redirect('back')
+  // }
 })
 
 module.exports = router
