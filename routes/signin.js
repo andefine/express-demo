b93756a7c848c2db3d36ea87f8099de412bdf8be
andefine <1939866551@qq.com>
2018-12-05 09:46:56 +0800

    登录

--- routes/signin.js
+++ routes/signin.js
@@ -1,6 +1,8 @@
+const sha1 = require('sha1')
 const express = require('express')
 const router = express.Router()
 
+const UserModel = require('../models/users')
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 router.get('/', checkNotLogin, function (req, res, next) {
@@ -8,18 +10,37 @@ router.get('/', checkNotLogin, function (req, res, next) {
 })
 
 router.post('/', checkNotLogin, function (req, res, next) {
-  res.send('登录')
-  // const name = req.fields.name
-  // const password = req.fields.password
+  const name = req.fields.name
+  const password = req.fields.password
 
-  // try {
-  //   if (!name.length) {
-  //     throw new Error('请填写用户名')
-  //   }
-  // } catch (e) {
-  //   req.flash('error', e.message)
-  //   return res.redirect('back')
-  // }
+  try {
+    if (!name.length) {
+      throw new Error('请填写用户名')
+    }
+    if (!password.length) {
+      throw new Error('请填写密码')
+    }
+  } catch (e) {
+    req.flash('error', e.message)
+    return res.redirect('back')
+  }
+
+  UserModel.getUserByName(name)
+  .then((user) => {
+    if (!user) {
+      req.flash('error', '用户不存在')
+      return res.redirect('back')
+    }
+    if (sha1(password) !== user.password) {
+      req.flash('error', '用户名或密码错误')
+      return res.redirect('back')
+    }
+    req.flash('success', '登录成功')
+    delete user.password
+    req.session.user = user
+    res.redirect('/posts')
+  })
+  .catch(next)
 })
 
 module.exports = router
