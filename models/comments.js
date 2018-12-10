8c9155cf3b8b825bb2e8ec1122a2b89031dd13c8
andefine <1939866551@qq.com>
2018-12-10 21:46:11 +0800

    fix

---
+++ models/comments.js
@@ -0,0 +1,11 @@
+// const marked = require('marked')
+// const Comment = require('../lib/mongo').Comment
+
+// Comment.plugin('contentToHtml', {
+//   afterFind (comments) {
+//     return comments.map((comment) => {
+//       comment.content = marked(comment.content)
+//       return comment
+//     })
+//   }
+// })
